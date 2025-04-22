ad582179b0f5f8aea6531366a73efe4523e38193
Ashirwad-Ingle <ashirwadingle34@gmail.com>
2025-04-22 10:17:58 +0530

    add gitingore

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
 import express from "express";
+import cors from "cors";
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`server is running on PORT ${process.env.PORT}`)
+})
+
